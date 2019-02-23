6a3a5ef2f1517d8151b5d13e15b181bff7508666
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-23 01:25:59 -0500

    Add Snack class with constructor

---
+++ src/javaSnackBar/Snack.java
@@ -0,0 +1,19 @@
+package javaSnackBar;
+
+public class Snack {
+  private static int maxId = 0;
+  private int id;
+  private String name;
+  private int quantity;
+  private double cost;
+  private int vendingMachineId;
+
+  public Snack (String name, int quantity, double cost, int vendingMachineId) {
+    maxId++;
+    id = maxId;
+    this.name = name;
+    this.quantity = quantity;
+    this.cost = cost;
+    this.vendingMachineId = vendingMachineId;
+  }
+}
