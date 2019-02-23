8a9236cf8a4eb7816beb472d67a2f0386e59f224
Andres Rivera <andres.rivera.toro@gmail.com>
2019-02-23 01:34:55 -0500

    Add VendingMachine class with constructor

---
+++ src/javaSnackBar/VendingMachine.java
@@ -0,0 +1,13 @@
+package javaSnackBar;
+
+public class VendingMachine {
+  private static int maxId = 0;
+  private int id;
+  private String name;
+
+  public VendingMachine(String name) {
+    maxId++;
+    id = maxId;
+    this.name = name;
+  }
+}
